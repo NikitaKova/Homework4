a3ba21eb476db03fe609cfa2bba1b2a5f9f25e4f
NikitaKovalev <nik.kovalev.00@bk.ru>
2022-10-17 21:25:08 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+//1, 2, 5, 7, 19, 6, 7, 8
+
+
+int [] CreateMassiv()
+{
+    int [] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,10);
+        Console.Write($"{array[i]} ");
+    }
+    return array;
+} 
+
+//void PrintArray(int[] array)
+//{
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        Console.Write($"{array[i]}");
+//    }
+//}
+int[] arr = CreateMassiv();
+//PrintArray(arr);
